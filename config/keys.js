6c9bdc2b2923ce23574f951331faeb1f30b75e71
Theresa <TreeGee73@gmail.com>
2021-01-21 15:23:05 -0700

    backend auth built -tested (passed) postman

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: "mongodb+srv://treegee73:CJPM83ROP6wqW685@users.gd3ve.mongodb.net/users?retryWrites=true&w=majority" 
-  };
+    mongoURI: "mongodb+srv://treegee73:CJPM83ROP6wqW685@users.gd3ve.mongodb.net/users?retryWrites=true&w=majority",
+    secretOrKey: "secret"
+};
