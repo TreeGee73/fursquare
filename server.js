99c8036f225f82f9893b4c4567cec1a7411c5ab2
Theresa <TreeGee73@gmail.com>
2021-01-21 20:14:50 -0700

    updates

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const passport = require("passport");
 
-const users = require("./routes/api/users");
+const users = require("./authentication/routes/api/users");
 
 const app = express();
 
@@ -16,7 +16,7 @@ app.use(
 app.use(bodyParser.json());
 
 // DB Config
-const db = require("./config/keys").mongoURI;
+const db = require("./authentication/config/keys").mongoURI;
 
 // Connect to MongoDB
 mongoose
@@ -28,10 +28,10 @@ mongoose
 app.use(passport.initialize());
 
 // Passport config
-require("./config/passport")(passport);
+require("./authentication/config/passport")(passport);
 
 // Routes
-app.use("/api/users", users);
+app.use("./authentication/routes/api/users", users);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}!`));
