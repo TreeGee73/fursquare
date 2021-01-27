9d3d2a7121088d2aab5024e8ae589f21bb598d1b
Alan Huynh <alan.hanhuynh@gmail.com>
2021-01-27 01:36:40 -0700

    search-functionality

--- client/src/components/layout/services.jsx
+++ client/src/components/layout/services.jsx
@@ -1,20 +1,50 @@
+// import { json } from "body-parser";
 import React, { Component } from "react";
 
 class Search extends Component {
 
-    state = {};
+    state = {
+        search: '',
+        dogs: []
+    };
+
+    handleOnChange = event => {
+        this.setState({ search: event.target.value });
+    };
+
+    handleSearch = () => {
+        this.makeApiCall(this.state.search);
+    };
+
+    makeApiCall = searchInput => {
+        const searchUrl = '';
+        fetch(searchUrl)
+            .then(response => {
+                return response.json();
+            })
+            .then(jsonData => {
+                this.setState({ dogs: jsonData.dogs });
+            });
+    };
 
     render() {
 
         return (
 
-            <div>
+            <div id="main">
                 <h1>Welcome to FurSearch!</h1>;
-                <input name="text" type="text" placeholder="Search" />
-                <button>Search</button>
-            </div>
+                <input name="text" type="text" placeholder="Search" onChange={event => this.handleOnChange(event)} value={this.state.search} />
+                <button onClick={this.handleSearch}>Search</button>
+                {/* {this.state.dogs ? (
+                    <div id="dogs-container">
+                        {this.state.dogs.map((dog, index) => (
+                            <div class="single-dog" key={index}>
+                                <h2>{dog.strDog}</h2>
 
-        );
+            ))} */}
+                            </div>
+
+                        );
     }
 }
 
