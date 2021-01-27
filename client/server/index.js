67cac61e270e7ff543ceec461aeaf7f6aa43465f
Tucker <tuckerbeauchamp@gmail.com>
2021-01-27 12:43:48 -0700

    remove server from client directory

--- client/server/index.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const app = express();
-const config = require("./config");
-
-// endpoints
-const ImageController = require("./controllers/imageController");
-app.post("/api/uploadImage", ImageController.uploadImageToS3);
