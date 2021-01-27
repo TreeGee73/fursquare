622f2cd1f7e4197bce171f60413da1234f785f42
Brandt <frickerbrandt@gmail.com>
2021-01-27 13:21:42 -0700

    got google search to work, having issues accesing the returned data

--- client/src/components/layout/services.jsx
+++ client/src/components/layout/services.jsx
@@ -1,51 +1,59 @@
-// import { json } from "body-parser";
 import React, { Component } from "react";
 
+
 class Search extends Component {
+  state = {
+    searchValue: "",
+    info: []
+  };
 
-    state = {
-        search: '',
-        dogs: []
-    };
+  handleOnChange = event => {
+    this.setState({ searchValue: event.target.value });
+  };
 
-    handleOnChange = event => {
-        this.setState({ search: event.target.value });
-    };
+  handleSearch = () => {
+    this.makeApiCall(this.state.searchValue);
+  };
 
-    handleSearch = () => {
-        this.makeApiCall(this.state.search);
-    };
+  makeApiCall = searchInput => {
+    var searchUrl = `https://www.googleapis.com/customsearch/v1?key=AIzaSyDjA6PJZpa-H7l3BevZHUGl8sMBUxGrP7M&cx=3142a4046227fd8fe&q=${searchInput}`;
+    fetch(searchUrl)
+      .then(response => {
+          console.log(response.json())
+        // return response.json();
+      })
+      .then(jsonData => {
+        this.setState({ info: jsonData.info });
+      });
+  };
 
-    makeApiCall = searchInput => {
-        const searchUrl = '';
-        fetch(searchUrl)
-            .then(response => {
-                return response.json();
-            })
-            .then(jsonData => {
-                this.setState({ dogs: jsonData.dogs });
-            });
-    };
-
-    render() {
-
-        return (
-
-            <div id="main">
-                <h1>Welcome to FurSearch!</h1>;
-                <input name="text" type="text" placeholder="Search" onChange={event => this.handleOnChange(event)} value={this.state.search} />
-                <button onClick={this.handleSearch}>Search</button>
-                {/* {this.state.dogs ? (
-                    <div id="dogs-container">
-                        {this.state.dogs.map((dog, index) => (
-                            <div class="single-dog" key={index}>
-                                <h2>{dog.strDog}</h2>
-
-            ))} */}
-                            </div>
-
-                        );
-    }
+  render() {
+    return (
+      <div id="main">
+        <h1>Services</h1>
+        <input
+          name="text"
+          type="text"
+          placeholder="Search for Services"
+          onChange={event => this.handleOnChange(event)}
+          value={this.state.searchValue}
+        />
+        <button onClick={this.handleSearch}>Enter</button>
+        {this.state.meals ? (
+          <div id="meals-container">
+            {this.state.meals.map((meal, index) => (
+              <div className="single-meal" key={index}>
+                <h2>{meal.strMeal}</h2>
+                <img src={meal.strMealThumb} alt="meal-thumbnail" />
+              </div>
+            ))}
+          </div>
+        ) : (
+          <p>Try searching for a meal</p>
+        )}
+      </div>
+    );
+  }
 }
 
 export default Search;
