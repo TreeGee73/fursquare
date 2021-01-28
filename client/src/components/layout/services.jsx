1396250a7ecdbea1718371fea6e1fe714520f639
Brandt <frickerbrandt@gmail.com>
2021-01-28 08:13:36 -0700

    mapped over items to display cards

--- client/src/components/layout/services.jsx
+++ client/src/components/layout/services.jsx
@@ -1,8 +1,18 @@
 import React, { Component } from "react";
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
 
 
+  
 
 class Search extends Component {
+    
   state = {
     searchValue: "",
     items: []
@@ -61,8 +71,25 @@ class Search extends Component {
           <div id="items-container">
             {this.state.items.map((item, index) => (
               <div class="single-meal" key={index}>
-                <h2>{item.displayLink}</h2>
-                {/* <img src={meal.strMealThumb} alt="meal-thumbnail" /> */}
+                {/* <h2>{item.displayLink}</h2> */}
+                <Card>
+      <CardActionArea>
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            {item.displayLink}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
+            across all continents except Antarctica
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary"><a href={item.formattedUrl}>Learn More</a>
+          
+        </Button>
+      </CardActions>
+    </Card>
               </div>
             ))}
           </div>
@@ -70,29 +97,6 @@ class Search extends Component {
           <p>Try searching for a meal</p>
         )}
          
-
-
-
-
-        {/* <MediaCard info={this.state.info[0]}/> */}
-        {/* {this.state.items 
-        !== []? (
-          <div id="info-container">
-              
-            {this.state.items.map((item, index) => (
-              <div className="single-info" 
-              key={index}
-              >
-                <h2>{item}</h2> */}
-                {/* <img src={meal.strMealThumb} alt="meal-thumbnail" /> */}
-
-                
-              {/* </div>
-            ))}
-          </div>
-        ) : (
-          <p>Oooops</p>
-        )} */}
       </div>
     );
   }
