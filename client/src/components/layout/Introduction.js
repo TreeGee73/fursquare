49c99511b64e53c856e9f361f93195f1d718c2f3
Theresa <TreeGee73@gmail.com>
2021-01-28 16:06:00 -0700

    fixing file name issues

---
+++ client/src/components/layout/Introduction.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react'
+
+export default class Introduction extends Component {
+  render() {
+    return (
+      <div>
+        <section id="colorlib-hero" className="js-fullheight" data-section="home">
+          <div className="flexslider js-fullheight">
+            <ul className="slides">
+              <li style={{backgroundImage: 'url(images/cover_bg_1.jpg)'}}>
+                <div className="overlay" />
+                <div className="container-fluid">
+                  <div className="row">
+                    <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
+                      <div className="slider-text-inner js-fullheight">
+                        <div className="desc">
+                          <h1>Welcome to <br />FurSquare!</h1>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </li>
+              <li style={{backgroundImage: 'url(images/img_bg_2.jpg)'}}>
+                <div className="overlay" />
+                <div className="container-fluid">
+                  <div className="row">
+                    <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
+                      <div className="slider-text-inner">
+                        <div className="desc">
+                          <h1 className="h1-home">A site for<br/>pet owners<br/>and their pets...</h1>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </li>
+              <li style={{backgroundImage: 'url(images/img_bg_1.jpg)'}}>
+                <div className="overlay" />
+                <div className="container-fluid">
+                  <div className="row">
+                    <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
+                      <div className="slider-text-inner">
+                        <div className="desc">
+                          <h1>To find services<br/>and connect<br/>with other pet owners.</h1>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </section>
+      </div>
+    )
+  }
+}
---
+++ client/src/components/layout/Services.js
@@ -0,0 +1,102 @@
+import React, { Component } from "react";
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const { REACT_APP_GOOGLE_API_KEY, REACT_APP_GOOGLE_CX } = process.env;
+
+
+class Search extends Component {
+
+  state = {
+    searchValue: "",
+    items: [],
+  };
+
+  handleOnChange = (event) => {
+    this.setState({ searchValue: event.target.value });
+  };
+
+  handleSearch = () => {
+    this.makeApiCall(this.state.searchValue);
+  };
+
+  makeApiCall = searchInput => {
+    var searchUrl = "https://www.googleapis.com/customsearch/v1?key=" + REACT_APP_GOOGLE_API_KEY + "&cx=" + REACT_APP_GOOGLE_CX + "=" + searchInput;
+    fetch(searchUrl)
+      .then(response => {
+
+        return response.json();
+      })
+      .then(jsonData => {
+             this.setState({
+            items: jsonData.items,
+            searchValue: ""})
+           console.log(jsonData)
+            console.log(jsonData.items[0])
+
+
+
+
+
+      });
+
+
+      }
+
+
+
+
+
+
+
+  render() {
+    return (
+      <div id="main" style={{ backgroundImage: `url(${process.env.PUBLIC_URL + "/images/login_1.jpg"})`}}>
+        <h1>Top Hit Service</h1>
+        <input
+          name="text"
+          type="text"
+          placeholder="Search for Service"
+          onChange={(event) => this.handleOnChange(event)}
+          value={this.state.searchValue}
+        />
+        <button onClick={this.handleSearch}>Enter</button>
+        {this.state.items !== [] ? (
+          <div id="items-container">
+            {this.state.items.map((item, index) => (
+              <div class="single-meal" key={index}>
+
+                <Card style={{height: "140px", maxWidth: "345px" ,border: "1px solid", marginTop: "10px", marginBottom: "10px", display: "block", marginLeft: "auto", marginRight: "auto"}}>
+      <CardActionArea>
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            {item.displayLink}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            {item.title}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary"><a href={item.formattedUrl}>Learn More</a>
+
+        </Button>
+      </CardActions>
+    </Card>
+              </div>
+            ))}
+          </div>
+        ) : (
+          <p>You need help! This search is powered by Google</p>
+        )}
+      <div style={{ backgroundImage: `url(${process.env.PUBLIC_URL + "/images/login_1.jpg"})`, height: "100vh"}}></div>
+      </div>
+    );
+  }
+}
+
+export default Search;
