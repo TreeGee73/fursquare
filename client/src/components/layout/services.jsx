ba4e6f5298a25e194f086c9d4b5fcbb7eb7c92c9
Brandt <frickerbrandt@gmail.com>
2021-01-28 08:01:56 -0700

    got stuff rendered

--- client/src/components/layout/services.jsx
+++ client/src/components/layout/services.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
 
 
+
 class Search extends Component {
   state = {
     searchValue: "",
-    info: []
+    items: []
   };
 
   handleOnChange = event => {
@@ -19,39 +20,79 @@ class Search extends Component {
     var searchUrl = `https://www.googleapis.com/customsearch/v1?key=AIzaSyDjA6PJZpa-H7l3BevZHUGl8sMBUxGrP7M&cx=3142a4046227fd8fe&q=${searchInput}`;
     fetch(searchUrl)
       .then(response => {
-        //   console.log(response.json())
+        
         return response.json();
       })
       .then(jsonData => {
-        // this.setState({ info: jsonData.info });
-        console.log(jsonData)
+             this.setState({ 
+            items: jsonData.items, 
+            searchValue: ""})
+           console.log(jsonData)
+            console.log(jsonData.items[0])
+
+
+           
+        
+        
       });
-  };
+      
+      
+      }
+
+
+
+
+ 
+
 
   render() {
     return (
       <div id="main">
-        <h1>Services</h1>
+        <h1>Top Hit Service</h1>
         <input
           name="text"
           type="text"
-          placeholder="Search for Services"
+          placeholder="Search for Service"
           onChange={event => this.handleOnChange(event)}
           value={this.state.searchValue}
         />
         <button onClick={this.handleSearch}>Enter</button>
-        {this.state.meals ? (
-          <div id="meals-container">
-            {this.state.meals.map((meal, index) => (
-              <div className="single-meal" key={index}>
-                <h2>{meal.strMeal}</h2>
-                <img src={meal.strMealThumb} alt="meal-thumbnail" />
+        {this.state.items !== [] ? (
+          <div id="items-container">
+            {this.state.items.map((item, index) => (
+              <div class="single-meal" key={index}>
+                <h2>{item.displayLink}</h2>
+                {/* <img src={meal.strMealThumb} alt="meal-thumbnail" /> */}
               </div>
             ))}
           </div>
         ) : (
-          <p>Try a better Search</p>
+          <p>Try searching for a meal</p>
         )}
+         
+
+
+
+
+        {/* <MediaCard info={this.state.info[0]}/> */}
+        {/* {this.state.items 
+        !== []? (
+          <div id="info-container">
+              
+            {this.state.items.map((item, index) => (
+              <div className="single-info" 
+              key={index}
+              >
+                <h2>{item}</h2> */}
+                {/* <img src={meal.strMealThumb} alt="meal-thumbnail" /> */}
+
+                
+              {/* </div>
+            ))}
+          </div>
+        ) : (
+          <p>Oooops</p>
+        )} */}
       </div>
     );
   }
