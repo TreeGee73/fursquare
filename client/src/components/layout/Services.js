cd01d560991916c87af4e3070794feb53c591d37
Theresa <TreeGee73@gmail.com>
2021-01-28 21:00:30 -0700

    images again

--- client/src/components/layout/Services.js
+++ client/src/components/layout/Services.js
@@ -2,9 +2,9 @@ import React, { Component } from "react";
 
 import Card from "@material-ui/core/Card";
 import CardActionArea from "@material-ui/core/CardActionArea";
-import CardActions from "@material-ui/core/CardActions";
+// import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
-import Button from "@material-ui/core/Button";
+// import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import CardMedia from "@material-ui/core/CardMedia";
 
